58fc5a770a51ce429d760873e575c37fbdc3704c
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-01-04 15:20:32 -0800

    hello world

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+Message = "\"Programming is like building a multilingual puzzle"
+
+print(Message)
---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+number = 98
+
+Message = "Battery street"
+
+print(f"{number} {Message} ")
