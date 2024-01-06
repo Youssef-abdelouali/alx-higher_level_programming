dfb3caa9a39d3c51519236b913d535616ebab2f5
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-01-06 07:28:47 -0800

    hello wolrd task

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
 number = 98
-
-Message = "Battery street"
-
-print(f"{number} {Message} ")
+print(f"{number} Battery street\n")
--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-out_put1 = str[0:6]
-out_put2 = str[39:66]
-out_put3 = str[107:112]
-print(out_put2 +' ' + out_put3 + out_put1)
+language that combines remarkable power with very clear syntax"
+str = str[39:66] + ' ' + str[106:112] + ' ' + str[:6]
+print(str)
