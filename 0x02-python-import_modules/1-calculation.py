006b591424b22e7ae1d610a80967cf21b5b43c45
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-01-09 07:24:32 -0800

    error fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, subtract(a, b)))
     print("{} * {} = {}".format(a, b, multiply(a, b)))
-    print("{} / {} = {:.2f}".format(a, b, divide(a, b)))
+    print("{} / {} = {}".format(a, b, divide(a, b)))
