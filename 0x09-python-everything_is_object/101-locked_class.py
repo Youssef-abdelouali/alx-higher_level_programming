a83f6a98c61a528d1a333038072f04ffcd337e1f
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-01-31 03:58:28 -0800

    advanced task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Defines a locked class."""
 
+
 class LockedClass:
     """
     A class that restricts the creation of new attributes to only 'first_name'.
+    for anything but attributes called 'first_name'.
     """
 
     __slots__ = ["first_name"]
