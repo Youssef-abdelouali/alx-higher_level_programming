18b79b73b27998642c4e790b963b37c9c3195b93
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-01-31 03:18:55 -0800

    advanced task

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.repet_count = getattr(magic_string, 'repet_count', 0) + 1
-    return "BestSchool" * magic_string.repet_count
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return "BestSchool" * magic_string.counter
