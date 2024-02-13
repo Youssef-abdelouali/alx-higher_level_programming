20f982d2e6135df44277a3ba12914dd9dda785be
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-02-13 06:20:06 -0800

    tests tasks

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" a function that prints a text with 2 new
+lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
+    while c < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ".?:":
+                print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
