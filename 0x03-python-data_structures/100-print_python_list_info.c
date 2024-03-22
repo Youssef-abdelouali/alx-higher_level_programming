daefbdca986ee78495b55caf4b6ce028e1d75fb7
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-03-22 10:18:41 -0700

    data tasks

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,12 +4,12 @@
 
 void print_python_list_info(PyObject *p)
 {
-    long int list_size = PyList_Size(p);
-    int index;
-    PyListObject *list_obj = (PyListObject *)p;
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-    printf("[*] Size of the Python List = %li\n", list_size);
-    printf("[*] Allocated = %li\n", list_obj->allocated);
-    for (ix = 0; ix < list_size; ix++)
-        printf("Element %i: %s\n", ix, Py_TYPE(list_obj->ob_item[ix])->tp_name);
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
