b06d1053b6855615cf9c89f79678330008cabd4b
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-04-12 06:34:59 -0700

    js tasks

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// Use console.log(...) to print all output
+// Use console.log(...) to print three lines of text
 console.log("C is fun");
 console.log("Python is cool");
 console.log("JavaScript is amazing");
