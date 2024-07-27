f74db0562da96dd1da931f1ee58d5296dfd3c694
Youssef Abdelouali <youssefabdelouali255@gmail.com>
2024-07-27 06:34:03 -0700

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 # Sends a request to a URL and displays the size of the response body in bytes
 curl -s "$1" | wc -c
-=======
-# This script gets the size of the response body from a given URL.
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
->>>>>>> e291a603555a68d957db458d21db0f80d36d3e20
