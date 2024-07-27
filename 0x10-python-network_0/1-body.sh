45b60cd09a8808c59543393f68aa68ef60f55e8d
Youssef Abdelouali <youssefabdelouali255@gmail.com>
2024-07-27 06:48:07 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body of a 200 status code response
-response=$(curl -s -w "%{http_code}" "$1")
-body=$(echo "$response" | head -n1)
-status_code=$(echo "$response" | tail -n1)
-
-if [ "$status_code" -eq 200 ]; then
-  echo "$body"
-fi
+response=$(curl -s -w "%{http_code}" -o temp_response.txt "$1")
+if [ "${response: -3}" -eq 200 ]; then cat temp_response.txt; fi
