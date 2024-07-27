539efa8a2a14944ad5083570076c4b17677cf07b
Youssef Abdelouali <youssefabdelouali255@gmail.com>
2024-07-27 08:03:44 -0700

    tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of a 200 status code response
-curl -s -o response.txt -w "%{http_code}" "$1" | grep -q "200" && cat response.txt
+# Sends a GET request to the URL and displays only the body of the response for a 200 status code
+curl -s "$1" -o /tmp/response.txt
+grep -q "200" <(curl -s -o /dev/null -w "%{http_code}" "$1") && cat /tmp/response.txt
