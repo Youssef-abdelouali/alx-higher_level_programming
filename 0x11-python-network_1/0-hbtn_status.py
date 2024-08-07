3eaec0ac1fec9ba3178a1e561b1d5732e1214d5b
Youssef Abdelouali <youssefabdelouali255@gmail.com>
2024-08-07 04:53:06 -0700

    tasks network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status
+A script to fetch and display the response from https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
 
 def main():
     """
-    Funtion to print a response of a specific url
+    Function to display the response from https://alx-intranet.hbtn.io/status
     """
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
 
 if __name__ == "__main__":
     main()
